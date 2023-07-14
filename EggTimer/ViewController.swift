a4c8f6e9cf1fa5ae125ed86eb2c075f50ad1e5cc
Kelvin C. Ricafort <kelvs.r@icloud.com>
2023-07-14 22:17:14 +0800

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Kelvin Ricafort on 7/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
